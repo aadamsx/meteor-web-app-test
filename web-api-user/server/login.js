e02c2b8d127722214221c94fa38f31d3b1ae39dd
aadamsx <aadams99@gmail.com>
2017-07-28 12:44:33 -0500

    updates to token code

--- web-api-user/server/login.js
+++ web-api-user/server/login.js
@@ -5,12 +5,12 @@ import { HTTP } from 'meteor/http';
 
 const getBearerTokenWithLoginToken = loginToken => {
   console.log(`getBearerTokenWithLoginToken called`);
-  console.log(`loginToken: ${loginToken}, databaseId: ${'database_1'}`)
+  console.log(`loginToken: ${loginToken}, databaseId: ${Meteor.settings.databaseId}`)
   let result = null;
   try {
     result = HTTP.call('POST', 'http://localhost:4500/users/token-login',
       {
-        data: { dbId: 'database_1', loginToken: loginToken },
+        data: { dbId: Meteor.settings.databaseId, loginToken: loginToken }, // NOTE: This will search over the entire users collection to find the token, no need to pass in the userId
         headers: { 'content-type': 'application/json', 'cache-control': 'no-cache' }
       }
     );
@@ -36,30 +36,24 @@ const ServerState = new Mongo.Collection('ServerState');
 Accounts.onLogin(user => {
   const userId = user && user.user && user.user._id || '';
 	console.log(`Accounts.onLogin on server is called, userId: ${userId}`);
-  const tokenRecord = ServerState.findOne({ type: 'WEB_API_TOKEN' });
-  const token = tokenRecord && tokenRecord.token || null;
-  const tokenExpires = tokenRecord && tokenRecord.tokenExpires || null;
+  const tokenRecord = ServerState.findOne({ type: 'WEB_API_TOKEN', userId: userId }); // NOTE: Each user has his own Bearer token
+  const token = tokenRecord && tokenRecord.token || null; // Bearer token
+  const tokenExpires = tokenRecord && tokenRecord.tokenExpires || null; // Bearer token expire date
   let getNewBearerToken = false;
 
   // => check to see if this user already has a [Bearer] token stored...
-  // the token is the Bearer token and the timestamp is the Bearer token validity window.
-  // debugger;
   if (token) { // => if the object exists in MongoDB storage, check the timestamp to see if we are still within valid range...
     debugger;
     const result = moment(new Date()).diff(tokenExpires, 'minutes');
     console.log(`check if we are within the timestamp window, proceed with using the existing [Bearer] token from MongoDB storage and do NOTHING more here.`);
-    // if we are outside of the timestamp window, get a new [login] token from the Web API.
+    // => if we are outside of the timestamp window, get a new [login] token from the Web API.
     if (result > 0) {
       console.log(`out of window, get new token.`);
       getNewBearerToken = true;
     }
-    else {
-      // proceed with using the existing [Bearer] token in localStorage and do NOTHING more here.
-      console.log(`within window of token, just use the token we already have.`);
-      getNewBearerToken = false;
-    }
+    // else {} // => proceed with using the existing [Bearer] token found in the MongoDB ServerState collection and do NOTHING more here.
   }
-  else getNewBearerToken = true; // => if not get a new [login] token from the Web API.
+  else getNewBearerToken = true; // => if not send a new temporary, one-time [login] token to the Web API.
 
 
   if (getNewBearerToken) {
@@ -71,7 +65,7 @@ Accounts.onLogin(user => {
     const response = getBearerTokenWithLoginToken(loginToken);
     debugger;
     console.log(`results should contain a [Bearer] token AND a timestamp, save these two values to MongoDB storage somewhere.`);
-    ServerState.update({ type: 'WEB_API_TOKEN' }, { type: 'WEB_API_TOKEN', token: response.data.token, tokenExpires: response.data.tokenExpires }, { upsert: true, multi: false }); // => tokenExpires: ISO encoded date string
-    // console.log(`all subsequent calls to the Web API must use the [Bearer] token stored in localStorage.`);
+    ServerState.update({ type: 'WEB_API_TOKEN' }, { type: 'WEB_API_TOKEN', userId: userId, token: response.data.token, tokenExpires: response.data.tokenExpires }, { upsert: true, multi: false }); // => tokenExpires: ISO encoded date string
+    // => console.log(`all subsequent calls to the Web API should use the [Bearer] token stored in localStorage.`);
   }
 });
